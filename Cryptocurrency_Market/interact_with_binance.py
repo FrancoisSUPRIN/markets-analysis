dc87879e634cd385a6835ae4d873ac22f20e8d11
Francois SUPRIN <100069440+FrancoisSUPRIN@users.noreply.github.com>
2024-04-04 14:45:19 +0200

    this version try to get trades from binance websocket and store data into MarketCandle object. I've to find how to define when a candle start and End in different timeframe

--- Cryptocurrency_Market/interact_with_binance.py
+++ Cryptocurrency_Market/interact_with_binance.py
@@ -42,6 +42,20 @@ def adjust_timestamps_to_local(df, timestamp_column):
 class Trade:
     def __init__(self, res):
         self.event_type = res["e"]
+        self.event_time = res['E']
+        self.symbol = res["s"]
+        self.trade_id = res["t"]
+        self.price = float(res["p"])  # Convertir le prix en float
+        self.quantity = float(res["q"])  # Convertir la quantité en float
+        self.buyer_order_id = res["b"]
+        self.selle_order_id = res["a"]
+        self.trade_time = res['T']
+        self.is_the_buyer_the_market_maker = res["m"]
+        self.ignore = res["M"]
+        
+        """class Trade:
+    def __init__(self, res):
+        self.event_type = res["e"]
         self.event_time = datetime.datetime.fromtimestamp(res['E'] / 1000, tz=datetime.timezone.utc)
         self.event_time = self.event_time.astimezone(pytz.timezone('Europe/Paris'))
         self.symbol = res["s"]
@@ -53,4 +67,4 @@ class Trade:
         self.trade_time = datetime.datetime.fromtimestamp(res['T'] / 1000, tz=datetime.timezone.utc)
         self.trade_time = self.trade_time.astimezone(pytz.timezone('Europe/Paris'))
         self.is_the_buyer_the_market_maker = res["m"]
-        self.ignore = res["M"]
+        self.ignore = res["M"]"""
