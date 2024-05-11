4995bc8276587eb2b3c70ce76b6c0ed2231bc76a
Francois SUPRIN <100069440+FrancoisSUPRIN@users.noreply.github.com>
2024-05-11 16:10:16 +0200

    Implement version enabling profitability calculation for the Imbalances After Fall trading strategy, accounting for transaction fees but excluding take profit and stop loss strategy. Additionally, integrate functionality to retrieve the latest candle from Binance, fetch real-time trades, and aggregate them into a candle. This facilitates the addition of the candle to the historical candles list.

--- Cryptocurrency_Market/market_candles.py
+++ Cryptocurrency_Market/market_candles.py
@@ -1,5 +1,18 @@
 class MarketCandle:
-    def __init__(self, timestamp, open_price, high_price, low_price, close_price, volume, close_time, quote_asset_volume, number_of_trades, taker_buy_base_asset_volume, taker_buy_quote_asset_volume, ignore):
+    def __init__(self, 
+        timestamp, 
+        open_price, 
+        high_price, 
+        low_price, 
+        close_price, 
+        volume, 
+        close_time, 
+        quote_asset_volume, 
+        number_of_trades, 
+        taker_buy_base_asset_volume, 
+        taker_buy_quote_asset_volume, 
+        ignore,
+    ):
         self.timestamp = int(timestamp)
         self.open_price = float(open_price)
         self.high_price = float(high_price)
@@ -12,6 +25,27 @@ class MarketCandle:
         self.taker_buy_base_asset_volume = float(taker_buy_base_asset_volume)
         self.taker_buy_quote_asset_volume = float(taker_buy_quote_asset_volume)
         self.ignore = ignore
+        
+class MarketCandleV2:
+    def __init__(self, res):
+        self.timestamp = int(res[0])
+        self.open_price = float(res[1])
+        self.high_price = float(res[2])
+        self.low_price = float(res[3])
+        self.close_price = float(res[4])
+        self.volume = float(res[5])
+        self.close_time = int(res[6])
+        self.quote_asset_volume = float(res[7])
+        self.number_of_trades = int(res[8])
+        self.taker_buy_base_asset_volume = float(res[9])
+        self.taker_buy_quote_asset_volume = float(res[10])
+        self.ignore = res[11]
+        
+        # Example
+        # from market_candles import MarketCandleV2
+        # responses = client.get_historical_klines(symbol, interval_value, from_date)
+        # historical_candles_v2 = [MarketCandleV2(res) for res in responses]
+        # historical_candles_v2
 
 def create_market_candle_from_dataframe(row): # compatible with Binance Rest API
     return MarketCandle(
--- Cryptocurrency_Market/market_trades.py
+++ Cryptocurrency_Market/market_trades.py
@@ -1,4 +1,4 @@
-class Trade:
+class MarketTrade:
     def __init__(self, res):
         self.event_type = res["e"]
         self.event_time = res['E']
@@ -7,7 +7,7 @@ class Trade:
         self.price = float(res["p"])  # Convertir le prix en float
         self.quantity = float(res["q"])  # Convertir la quantité en float
         self.buyer_order_id = res["b"]
-        self.selle_order_id = res["a"]
+        self.seller_order_id = res["a"]
         self.trade_time = res['T']
         self.is_the_buyer_the_market_maker = res["m"]
         self.ignore = res["M"]
@@ -15,14 +15,13 @@ class Trade:
         """class Trade:
     def __init__(self, res):
         self.event_type = res["e"]
-        self.event_time = datetime.datetime.fromtimestamp(res['E'] / 1000, tz=datetime.timezone.utc)
-        self.event_time = self.event_time.astimezone(pytz.timezone('Europe/Paris'))
+        self.event_time = datetime.datetime.fromtimestamp(res['E'] / 1000, tz=datetime.timezone.utc).astimezone(pytz.timezone('Europe/Paris'))
         self.symbol = res["s"]
         self.trade_id = res["t"]
         self.price = float(res["p"])  # Convertir le prix en float
         self.quantity = float(res["q"])  # Convertir la quantité en float
         self.buyer_order_id = res["b"]
-        self.selle_order_id = res["a"]
+        self.seller_order_id = res["a"]
         self.trade_time = datetime.datetime.fromtimestamp(res['T'] / 1000, tz=datetime.timezone.utc)
         self.trade_time = self.trade_time.astimezone(pytz.timezone('Europe/Paris'))
         self.is_the_buyer_the_market_maker = res["m"]
